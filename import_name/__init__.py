cd6e196e6887cc920c9ad1b0f64eea532d199e7c
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2022-02-21 20:12:09 -0500

    purge: remove the `import_name` file
    Leftover from copying the template

--- import_name/__init__.py
+++
@@ -1,3 +0,0 @@
-from .import_name import PackageClassOrSomething
-
-__all__ = ["PackageClassOrSomething"]
--- import_name/import_name.py
+++
@@ -1,9 +0,0 @@
-"""
-Hello there!
-
-Put your code here.
-
-Tips:
-- When you are trying to import stuff from the current directory,
-  you need to use the syntax `from .filename import whatever`
-"""
