087ae1f718ee84f8c7f04ca696e260a038dfb77a
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2022-05-05 22:26:23 -0400

    feat: add a workaround to allow tasks in cogs

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-to be filled at a later date
+Simple example, triggers every 3 seconds:
+```python
+from interactions.ext.tasks import IntervalTrigger, create_task
+
+@create_task(IntervalTrigger(3))
+async def my_task():
+    print("hi")
+
+my_task.start()
+```
+
+Multiple conditions (example will be improved when more triggers are added)\
+The task will only run every 5 seconds because every trigger needs to be ready
+```python
+from interactions.ext.tasks import IntervalTrigger, create_task
+
+@create_task(IntervalTrigger(3), IntervalTrigger(5))
+async def my_task():
+    print("hi")
+
+my_task.start()
+```
+
+To have the task run when any trigger is ready, use the OrTrigger.\
+The task will run every 3 and 5 seconds (it may run twice occasionally due to how timing works)
+```python
+from interactions.ext.tasks import IntervalTrigger, OrTrigger, create_task
+
+@create_task(OrTrigger(IntervalTrigger(3), IntervalTrigger(5)))
+async def my_task():
+    print("hi")
+
+my_task.start()
+```
+
+Classes are special, due to how functions turn into methods and other python black magic\
+As such, you need to pass `self` when starting the task\
+Any args or kwargs passed to `Task.start()` will be prepended to the function
+```python
+from interactions import Extension
+from interactions.ext.tasks import IntervalTrigger, create_task
+
+class Cog(Extension):
+    def __init__(self, client):
+        self.method.start(self)
+
+    @create_task(IntervalTrigger(1))
+    async def method(self):
+        print(self)
+```
--- interactions/ext/tasks/tasks.py
+++ interactions/ext/tasks/tasks.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 from abc import ABC, abstractmethod
-from typing import Callable, Coroutine, List, Union
+from typing import Callable, Coroutine, List, Sequence, Optional, Dict, Any
 
 
 class Trigger(ABC):
@@ -10,10 +10,49 @@ class Trigger(ABC):
         """
         A method to wait until all conditions to fire are met
         """
-        ...
+        raise NotImplementedError
 
     def __await__(self):
         return self.wait_for_ready().__await__()
+
+    def __or__(self, other):
+        return OrTrigger(self, other) if isinstance(other, Trigger) else NotImplemented
+
+
+class OrTrigger(Trigger):
+    """Waits until any of its triggers are ready"""
+
+    def __init__(self, *triggers: Trigger):
+        self.triggers: List[Trigger] = list(triggers)
+        self.tasks: List[asyncio.Task] = []
+
+    async def wait_for_ready(self):
+        to_make: List[Trigger] = []
+        for trigger in self.triggers:
+            for task in self.tasks:
+                if task.trigger == trigger:
+                    break
+            else:
+                to_make.append(trigger)
+
+        for trigger in to_make:
+            task = asyncio.create_task(trigger.wait_for_ready())
+            task.trigger = trigger
+            self.tasks.append(task)
+
+        done, pending = await asyncio.wait(
+            self.tasks, return_when=asyncio.FIRST_COMPLETED
+        )
+
+        for task in done:
+            self.tasks.remove(task)
+
+    def __or__(self, other):
+        if not isinstance(other, Trigger):
+            return NotImplemented
+
+        self.triggers.append(other)
+        return self
 
 
 class IntervalTrigger(Trigger):
@@ -23,30 +62,34 @@ class IntervalTrigger(Trigger):
 
     async def wait_for_ready(self):
         await asyncio.sleep((self.last_fire + self.delay) - time.time())
+        self.last_fire = time.time()
 
 
 class Task:
-    def __init__(self, func, triggers: List[Trigger], *args, **kwargs):
+    def __init__(self, func, triggers: Sequence[Trigger], *args, **kwargs):
         self.func: Callable[..., Coroutine] = func
-        self.triggers: List[Trigger] = triggers
-        self.args = args
-        self.kwargs = kwargs
+        self.triggers: Sequence[Trigger] = triggers
+        self.args: Sequence[Any] = args
+        self.kwargs: Dict[str, Any] = kwargs
 
+        self.pre_args: Sequence = ()
+        self.pre_kwargs: Dict[str, Any] = {}
         self._running: asyncio.Event = asyncio.Event()
         self._loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
     async def fire(self):
-        for trigger in self.triggers:
-            trigger.last_fire = time.time()
-        res = await self.func(*self.args, **self.kwargs)
+        res = await self.func(*self.pre_args, *self.args, **self.pre_kwargs, **self.kwargs)
         return res
 
-    def start(self) -> asyncio.Task:
+    def start(self, *args, **kwargs) -> asyncio.Task:
         """
         The synchronous method to start a task loop
+        Any args/kwargs passed are prepended to the set args/kwargs
         :return: The asyncio.Task running the loop
         :rtype: asyncio.Task
         """
+        self.pre_args = args
+        self.pre_kwargs = kwargs
         self._running.set()
         return self._loop.create_task(self.run())
 
@@ -67,12 +110,12 @@ class Task:
         """
         self._running.clear()
 
+    async def __call__(self, *args, **kwargs):
+        return await self.func(*args, **kwargs)
 
-def create_task(triggers: Union[Trigger, List[Trigger]], *args, **kwargs):
-    if not isinstance(triggers, list):
-        triggers = [triggers]
 
+def create_task(*triggers: Trigger, **kwargs):
     def inner(func):
-        return Task(func, triggers, *args, **kwargs)
+        return Task(func, triggers, **kwargs)
 
     return inner
